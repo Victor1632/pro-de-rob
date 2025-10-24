94cc4316441977c6122c92d9d459694e0e6308bf
Victor  hugo da silva lima 2C <fimbau093@gmail.com>
2025-10-24 09:06:33 -0300

    Update atividade_matriz_victor.py

--- atividade_matriz_victor.py
+++ atividade_matriz_victor.py
@@ -20,3 +20,17 @@ maior_segunda = sorted(matriz[1])[-1]
 print(f"\nA) Soma dos pares: {soma_pares}")
 print(f"B) Soma da terceira coluna: {soma_coluna3}")
 print(f"C) Maior valor da segunda linha: {maior_segunda}")
+
+
+
+
+Relatório:
+O código do David está excelente: fácil de entender, com boas variáveis e formatação limpa.
+A estrutura com dois for para preencher e exibir a matriz é didática e ideal para quem está aprendendo.
+O uso de max(matriz[1]) para pegar o maior valor é simples e eficiente.
+
+A soma da terceira coluna foi feita manualmente (matriz[0][2] + matriz[1][2] + matriz[2][2]).
+Poderia ser substituída por sum(linha[2] for linha in matriz), tornando o código mais flexível caso o tamanho da matriz mudasse futuramente
+O código poderia ter pequenas mensagens extras para guiar o usuário (por exemplo, “Agora vamos calcular os resultados…”).
+
+
